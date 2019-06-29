3e0796c70223a19f9c4a51e0c7459429006e5c7b
Braden Mason <bradenemason@gmail.com>
2019-06-29 09:44:07 -0500

    friends are rendering now

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,14 +1,39 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getData } from "../actions";
 
 class FriendsList extends React.Component {
+  componentDidMount() {
+    this.props.getData();
+  }
   render() {
+    console.log(this.props.friends);
     return (
       <div>
         <h1>Friends List</h1>
+        <div>
+          {this.props.friends.map(friend => {
+            return (
+              <div>
+                <h3>{friend.name}</h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default FriendsList;
+const mapStateToProps = state => {
+  return {
+    friends: state.friends,
+    error: state.error,
+    errorStatusCode: state.errorStatusCode
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getData }
+)(FriendsList);
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,8 +1,11 @@
-import { LOGGING_IN } from "../actions";
+import { LOGGING_IN, FETCH_DATA_START, FETCH_DATA_SUCCESS } from "../actions";
 
 const initialState = {
   friends: [],
-  isLoggingIn: false
+  isLoggingIn: false,
+  fetchingData: false,
+  error: "",
+  errorStatusCode: null
 };
 
 const reducer = (state = initialState, action) => {
@@ -12,6 +15,21 @@ const reducer = (state = initialState, action) => {
         ...state,
         isLoggingIn: true
       };
+    case FETCH_DATA_START: {
+      return {
+        ...state,
+        error: "",
+        fetchingData: true
+      };
+    }
+    case FETCH_DATA_SUCCESS: {
+      return {
+        ...state,
+        error: "",
+        fetchingData: false,
+        friends: action.payload
+      };
+    }
     default:
       return state;
   }
